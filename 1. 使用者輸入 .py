44ffd1b8e218901ce98d5b76290c948d40e78eda
XiaoKu <160765600+XiaoKu1022@users.noreply.github.com>
2024-06-11 12:36:57 +0800

    更新 註解

--- 1. 使用者輸入 .py
+++ 1. 使用者輸入 .py
@@ -1,5 +1,5 @@
 """
-如果可以讓使用者輸入多次『抽獎項目』E.g. 49折、59折、69折 .....
+如果要讓使用者輸入多次『抽獎項目』E.g. 49折、59折、69折 .....
 那就需要使用 Python 的陣列功能了，
 每當使用者新輸入一個抽獎項目時，
 將將它加入進陣列中，
@@ -11,7 +11,7 @@ list = []       # 宣告 list 為 陣列
 
 while True:     # 重複執行
     enter = input("> ")     # 將 變數 enter 設為 使用者輸入內容
-    if len(enter) > 0:      # 如果 變數 enter 長度大於 0 (使用者輸入為空字串)
+    if len(enter) > 0:      # 如果 變數 enter 長度大於 0 (使用者輸入非空字串)
         list.append(enter)      # 將 變數 enter 加入 陣列 list
     else:                   # 如果 變數 enter 是空的 (使用者沒有輸入內容)
         break                   # 跳出 while 迴圈
