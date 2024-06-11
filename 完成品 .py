4dbb24e6b803c0b3fdbb9abf10ba178027322d13
XiaoKu <160765600+XiaoKu1022@users.noreply.github.com>
2024-06-11 12:48:39 +0800

    更新 註解

--- 完成品 .py
+++ 完成品 .py
@@ -16,9 +16,9 @@ list = []       # 宣告 list 為 陣列
 
 
 while True:     # 重複執行
-    enter = input("請輸入獎項 > ")   # 將 變數 enter 設為 使用者輸入內容
+    enter = input("請輸入獎項 > ")    # 將 變數 enter 設為 使用者輸入內容
     if len(enter) > 0:              # 如果 變數 enter 長度大於 0
-        list.append(enter)          # 將 變數 enter 加入 陣列 list
+        list.append(enter)                  # 將 變數 enter 加入 陣列 list
 
     else:       # 如果 變數 enter 是空的 (使用者沒有輸入內容)
         break                       # 跳出 while 迴圈
@@ -26,21 +26,21 @@ while True:     # 重複執行
 
 mode = int(input(
     "請選擇模式： \n \t1 : 可重複抽 \n \t2 : 不可重複抽\n 請輸入 [ 1 / 2 ] > ")) # 選擇模式
-                # \n <- 為換行 ； \t <- 為 Tab 縮排
+              # \n <- 為換行 ； \t <- 為 Tab 縮排
 
 
-if mode == 1:
+if mode == 1: # 可重複抽
     while True:    
         for i in range(len(list)*10):    # 動畫執行次數 = len(list)*10
             time.sleep(i/(len(list)*100))# 暫停設為 i / (len(list)*50)
             hit = random.choice(list)
-            print("\r抽獎中 ".format(i)+hit,end=" ")   # 顯示動畫
+            print("\r抽獎中 ".format(i)+hit,end=" ")    # 顯示動畫
 
-        print("\r恭喜中獎 ".format(i)+hit,end="\n")   # 顯示中獎
+        print("\r恭喜中獎 ".format(i)+hit,end="\n")  # 顯示中獎
         os.system("pause")              # 暫停，按下任意鍵繼續抽
 
 
-elif mode == 2:
+elif mode == 2: # 不重複抽
     while len(list) > 0:    # 如果 陣列 list 長度大於 0 
         for i in range(len(list)*10):    # 動畫執行次數 = len(list)*10
             time.sleep(i/(len(list)*100))# 暫停設為 i / (len(list)*50)
